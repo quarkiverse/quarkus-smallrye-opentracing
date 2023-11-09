7a91b2cb42f7bb17d31bb12ce5f7576f502392b8
Guillaume Smet <guillaume.smet@gmail.com>
2023-11-09 15:59:46 +0100

    Handle integrations in the extension
    They used to be handled in the other extensions but now that the
    extension has been moved to the Quarkiverse, we need to handle it in
    this very extension.
    
    Also fixes a few issues that had not been caught testing with 3.5.1 in
    passing.
    
    The tests won't pass until we move the build to 3.6.0.

--- deployment/src/main/java/io/quarkus/smallrye/opentracing/deployment/SmallRyeOpenTracingProcessor.java
+++ deployment/src/main/java/io/quarkus/smallrye/opentracing/deployment/SmallRyeOpenTracingProcessor.java
@@ -8,12 +8,15 @@ import jakarta.servlet.DispatcherType;
 import io.opentracing.Tracer;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
+import io.quarkus.bootstrap.classloading.QuarkusClassLoader;
 import io.quarkus.deployment.Capabilities;
 import io.quarkus.deployment.Capability;
-import io.quarkus.deployment.Feature;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.AdditionalIndexedClassesBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
+import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveMethodBuildItem;
 import io.quarkus.resteasy.common.spi.ResteasyJaxrsProviderBuildItem;
 import io.quarkus.resteasy.reactive.spi.CustomContainerResponseFilterBuildItem;
@@ -28,6 +31,8 @@ import io.smallrye.opentracing.contrib.interceptor.OpenTracingInterceptor;
 import io.smallrye.opentracing.contrib.jaxrs2.server.SpanFinishingFilter;
 
 public class SmallRyeOpenTracingProcessor {
+
+    private static final String FEATURE = "smallrye-opentracing";
 
     @BuildStep
     AdditionalBeanBuildItem registerBeans(BuildProducer<UnremovableBeanBuildItem> unremovableBeans) {
@@ -54,7 +59,7 @@ public class SmallRyeOpenTracingProcessor {
             BuildProducer<WriterInterceptorBuildItem> writerInterceptors,
             Capabilities capabilities) {
 
-        feature.produce(new FeatureBuildItem(Feature.SMALLRYE_OPENTRACING));
+        feature.produce(new FeatureBuildItem(FEATURE));
 
         additionalBeans.produce(new AdditionalBeanBuildItem(QuarkusSmallRyeTracingDynamicFeature.class));
         providers.produce(new ResteasyJaxrsProviderBuildItem(QuarkusSmallRyeTracingDynamicFeature.class.getName()));
@@ -79,6 +84,44 @@ public class SmallRyeOpenTracingProcessor {
             writerInterceptors.produce(
                     new WriterInterceptorBuildItem.Builder(
                             QuarkusSmallRyeTracingStandaloneContainerResponseFilter.class.getName()).build());
+        }
+    }
+
+    @BuildStep
+    void handleKafkaIntegration(BuildProducer<ReflectiveClassBuildItem> reflectiveClass, Capabilities capabilities) {
+        //opentracing contrib kafka interceptors: https://github.com/opentracing-contrib/java-kafka-client
+        if (!capabilities.isPresent(Capability.KAFKA)
+                || !QuarkusClassLoader.isClassPresentAtRuntime("io.opentracing.contrib.kafka.TracingProducerInterceptor")) {
+            return;
+        }
+
+        reflectiveClass.produce(ReflectiveClassBuildItem.builder("io.opentracing.contrib.kafka.TracingProducerInterceptor",
+                "io.opentracing.contrib.kafka.TracingConsumerInterceptor").methods()
+                .build());
+    }
+
+    @BuildStep
+    void handleMongoDBIntegration(BuildProducer<AdditionalIndexedClassesBuildItem> additionalIndexedClasses,
+            Capabilities capabilities) {
+        if (!capabilities.isPresent(Capability.MONGODB_CLIENT)) {
+            return;
+        }
+
+        additionalIndexedClasses.produce(new AdditionalIndexedClassesBuildItem(
+                "io.quarkus.smallrye.opentracing.runtime.integration.MongoTracingCommandListener"));
+    }
+
+    @BuildStep
+    void handleRestClientIntegration(
+            Capabilities capabilities,
+            BuildProducer<NativeImageResourceBuildItem> resource,
+            BuildProducer<ReflectiveClassBuildItem> reflectiveClass) {
+        if (capabilities.isPresent(Capability.REST_CLIENT) || capabilities.isPresent(Capability.REST_CLIENT_REACTIVE)) {
+            resource.produce(new NativeImageResourceBuildItem(
+                    "META-INF/services/org.eclipse.microprofile.rest.client.spi.RestClientListener"));
+            reflectiveClass
+                    .produce(ReflectiveClassBuildItem.builder("io.smallrye.opentracing.SmallRyeRestClientListener")
+                            .methods().fields().build());
         }
     }
 }
